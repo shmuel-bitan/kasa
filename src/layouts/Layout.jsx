57a9a3bb9109bb31b8f62da8b24272cb742e2a86
Shmuel_B <smsbitan@gmail.com>
2023-03-29 15:00:44 +0200

    first commit

---
+++ src/layouts/Layout.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Layout.css";
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+
+function Layout({children}) {
+    return (
+        <>
+            <Header />
+            <main>{children}</main>
+            <Footer />
+        </>
+    );
+}
+
+export default Layout;
